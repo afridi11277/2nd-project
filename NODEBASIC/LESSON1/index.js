6ef02c3b708b33fa2e064139a37748721b8d2d2d
Afridi <63310854+afridi11277@users.noreply.github.com>
2021-08-04 11:18:56 +0600

    Add files via upload

---
+++ NODEBASIC/LESSON1/index.js
@@ -0,0 +1,13 @@
+const fs = require("fs")
+fs.existsSync("demo2.txt" )
+const add(result) => {
+    if(result){
+        console.log(result)
+
+    }
+    else{
+        console.log(result)
+        
+    }
+
+}
